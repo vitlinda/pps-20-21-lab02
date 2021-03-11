50cbc5abef304bf2a052f75b65853bbb36e4df7b
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-11 14:30:30 +0100

    fibonacci tail recursive refactoring

--- src/u02/tasks/part3/Fibonacci.scala
+++ src/u02/tasks/part3/Fibonacci.scala
@@ -2,9 +2,15 @@ package u02.tasks.part3
 
 object Fibonacci {
 
-//  @annotation.tailrec
-  def fib(n: Int): Int = n match {
-    case 0 | 1 => n
-    case _ => fib(n-1) + fib(n-2)
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def _fib(n: Int, accumulator: Int, a: Int): Int = n match {
+      case 0 => accumulator
+      case _ => _fib(n - 1, a, accumulator + a)
+    }
+    _fib(n, 0, 1)
   }
+
+
 }
