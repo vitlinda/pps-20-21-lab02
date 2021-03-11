c2892f14fe91df069eb12edace0a0421c22b565f
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-11 13:31:09 +0100

    generic compose test

--- src/u02/tasks/part2/Composition.scala
+++ src/u02/tasks/part2/Composition.scala
@@ -2,6 +2,8 @@ package u02.tasks.part2
 
 object Composition {
   //compose(x => x -1, y => y * 2)(5)
+//  def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
 
+  def genericCompose[A](f: A => Int, g: Int => A): Int => Int = x => f(g(x))
 }
--- test/u02/tasks/part2/CompositionTest.scala
+++ test/u02/tasks/part2/CompositionTest.scala
@@ -7,8 +7,14 @@ class CompositionTest {
   val composition = Composition
 
   @Test def compositionTest() {
-    assertEquals(9, composition.compose(_-1, _*2)(5))
+    assertEquals(9, composition.compose(_ - 1, _ * 2)(5))
   }
+
+  @Test def genericComposeTest() {
+    assertEquals(7, composition.genericCompose[Int](x => x, _+2)(5))
+  }
+
+
 
 }
 
