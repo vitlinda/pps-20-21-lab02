c42ab22e7207a706727edc08135363d9811e197f
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-18 17:39:56 +0100

    fibonacci non tailrec added in comments

--- src/u02/tasks/part3/Fibonacci.scala
+++ src/u02/tasks/part3/Fibonacci.scala
@@ -2,6 +2,11 @@ package u02.tasks.part3
 
 object Fibonacci {
 
+// def fib(n: Int): BigInt = n match {
+//   case 0 => 0
+//   case 1 => 1
+//   case _ => fib(n-2) + fib(n-1)
+// }
 
   def fib(n: Int): Int = {
     @annotation.tailrec
